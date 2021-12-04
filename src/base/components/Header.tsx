f5180cbd8e4ea31ab03708b200a32b62eeff97ca
Taylor <muhrlinetaylor@yahoo.com>
2021-12-03 22:18:18 -0600

    e1m1: finished header

--- src/base/components/Header.tsx
+++ src/base/components/Header.tsx
@@ -1,9 +1,39 @@
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
+import { NavLink } from 'react-router-dom';
 
 export const Header = () => {
     return (
-        <Grid className="header">
-            hello hello
+        <Grid container className="header">
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">Home</Typography>
+                </NavLink>
+            </Grid>
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">All</Typography>
+                </NavLink>
+            </Grid>
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">One</Typography>
+                </NavLink>
+            </Grid>
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">Create</Typography>
+                </NavLink>
+            </Grid>
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">Update</Typography>
+                </NavLink>
+            </Grid>
+            <Grid xs={2} className="headerLinkContainer">
+                <NavLink to="/" className="headerLink">
+                    <Typography variant="h5" className="headerLink_typography">Delete</Typography>
+                </NavLink>
+            </Grid>
         </Grid>
     )
 }
--- src/base/components/Landing.tsx
+++ src/base/components/Landing.tsx
@@ -1,11 +1,14 @@
+import { BrowserRouter as Router } from "react-router-dom"
 import { Header } from "./Header"
 
 export const Landing = () => {
     return (
         <div className="landing">
-            <div className="landingHeader">
-                <Header />
-            </div>
+            <Router>
+                <div className="landingHeader">
+                    <Header />
+                </div>
+            </Router>
         </div>
     )
 }
